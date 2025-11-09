fe43871f7c057c02d7385ada08554b533ce5f45d
swethamurugan64-pixel <swethamurugan64@gmail.com>
2025-11-09 20:30:16 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
 import mysql.connector
-import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT",3306))
+        host="localhost",
+        user="root",
+        password="Swetha*1243",
+        database="clinicdb"
     )
-    return connection
+                    
                                    
+
 
 
 
