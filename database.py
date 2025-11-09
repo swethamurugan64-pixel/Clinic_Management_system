daa10d89071a3acfac4977ab2cccc837ca516646
swethamurugan64-pixel <swethamurugan64@gmail.com>
2025-11-09 21:27:34 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,29 @@
 import mysql.connector
 import os
 
-def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
-    )
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT")
+)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS patients (
+  Patient_id INT AUTO_INCREMENT PRIMARY KEY,
+  Patient_name VARCHAR(100),
+  Contact VARCHAR(15),
+  Age INT,
+  Gender VARCHAR(10),
+  Medicine VARCHAR(50),
+  Quantity INT,
+  Total_price DECIMAL(10,2)
+)
+""")
+
+conn.commit()
+conn.close()
+print("✅ Table 'patients' created successfully!")
+
